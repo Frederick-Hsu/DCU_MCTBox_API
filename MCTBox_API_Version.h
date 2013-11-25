2c11ce0fcde64193184c44730a1d4e59726db17b
XU ZAN <zan.xu@hella.com>
2013-11-25 17:27:26 +0800

    Define  and list the version change log.

---
+++ MCTBox_API_Version.h
@@ -0,0 +1,25 @@
+/***********************************************************************************************************
+ * File name	: MCTBox_API_Version.h
+ * Description	: Define the current version of MCTBox_API library.
+ * Creator		: XU ZAN	[HSE-OT]
+ * Creation date: Mon.	Nov. 25, 2013
+ * Copyright(C)		2013	Hella Shanghai Electronics Co., Ltd.
+ *
+ ***********************************************************************************************************/
+
+#ifndef MCTBOX_API_VERSION_H
+#define MCTBOX_API_VERSION_H
+
+	#define YES	1
+	#define NO	0
+
+	#define UPGRADE_VERSION		YES
+/*****************************************************************/
+
+	#if (UPGRADE_VERSION == YES)
+		#define CURRENT_VERSION		"MCTBox_API Version : V0.0.0.1    Build-date : 2013-11-25"
+	#else
+		#define PREVIOUS_VERSION	"MCTBox_API Version : V0.0.0.0"
+	#endif	/*  UPGRADE_VERSION  */
+
+#endif  /* ndef MCTBOX_API_VERSION_H */

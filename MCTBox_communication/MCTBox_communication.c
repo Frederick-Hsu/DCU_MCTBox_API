c87379849e5b476bb0793b5479771c4ccaac0dae
XU ZAN <zan.xu@hella.com>
2014-01-13 10:45:16 +0800

    完成communication子模块的实现代码, 经测试通过.

--- MCTBox_communication/MCTBox_communication.c
+++ MCTBox_communication/MCTBox_communication.c
@@ -86,6 +86,74 @@ int MCTBox_communication_RS232_Exit(char *sARGOUT_ComError)
 /******************************************************************************/
 int MCTBox_communication_RS232_Write(char sARGIN_ComCmd2Write[], char *sARGOUT_ComError)
 {
-	int iError = 0;
-	return iError;
+	int iError = 0, iComWrtLen = 0;
+	char sError[256] = "";
+	
+	FlushInQ(g_iMCTBox_ComPortNr);
+	FlushOutQ(g_iMCTBox_ComPortNr);
+	
+	iComWrtLen = ComWrt(g_iMCTBox_ComPortNr, sARGIN_ComCmd2Write, strlen(sARGIN_ComCmd2Write));
+	if (iComWrtLen != strlen(sARGIN_ComCmd2Write))
+	{
+		iError = ReturnRS232Err();
+		strcpy(sError, GetRS232ErrorString(iError));
+		sprintf(sARGOUT_ComError, "Error code: %d, Error message: %s", iError, sError);
+		return iError;
+	}
+	
+	return iComWrtLen;
+}
+
+/******************************************************************************/
+int MCTBox_communication_RS232_Read(char *sARGOUT_ComResponse, char *sARGOUT_ComError)
+{
+	int iResult = 0, iCnt = 0, iError = 0;
+	char sResponse[256] = "", sError[256] = "";
+	
+#if 0
+	do
+	{
+		iResult = ComRd(g_iMCTBox_ComPortNr, sResponse+iCnt, 1);
+		if (iResult < 0)	// Eror occurs
+			break;
+		iCnt++;
+	}
+	while (sResponse[iCnt-1] != 0);	// Check until the NULL terminate char.
+#else
+	iResult = ComRd(g_iMCTBox_ComPortNr, sResponse, 256);
+#endif
+	if (iResult < 0)
+	{  
+		iError = ReturnRS232Err();
+		strcpy(sError, GetRS232ErrorString(iError));
+		sprintf(sARGOUT_ComError, "Error code: %d, Error message: %s", iError, sError);
+		
+		iResult = iError;
+	}
+	sprintf(sARGOUT_ComResponse, "%s", sResponse);
+	
+	return iResult;
+}
+
+/******************************************************************************/
+int MCTBox_communication_RS232_Query(char sARGIN_ComCmd2Write[], char *sARGOUT_ComResponse, char *sARGOUT_ComError)
+{
+	int iResult = 0, iLen = 0;
+	char sError[256] = "", sResponse[256] = "";
+	
+	iLen = MCTBox_communication_RS232_Write(sARGIN_ComCmd2Write, sError);
+	if (0 != strlen(sError))
+	{
+		sprintf(sARGOUT_ComError, "%s", sError);
+		return iLen;	// Negative error code
+	}
+	iLen = MCTBox_communication_RS232_Read(sResponse, sError);
+	if (0 != strlen(sError))
+	{
+		sprintf(sARGOUT_ComError, "%s", sError);
+		return iLen;	// Negative error code
+	}
+	sprintf(sARGOUT_ComResponse, "%s", sResponse);	// Output the response
+	
+	return iResult;
 }

3311f8b3916e1e4c028b1c89c7ae9c55a99954b5
XU ZAN <zan.xu@hella.com>
2014-06-03 14:56:25 +0800

    Replace the global variable "PSwitchLNode_t pSwitchNode" with "extern PDioPortNode_t pDioPortNode".  Why?
    Because we want to uniform the port/channel definitions for Dout board, Din channel, Fin board, Switch board,  to provide the sole token-definition-source.

--- MCTBox_SwitchControl/Switch_Link_Node.c
+++ MCTBox_SwitchControl/Switch_Link_Node.c
@@ -9,12 +9,22 @@
  ****************************************************************************************************/
 
 //==============================================================================
+// Macros :
+#define SHARE_DIO_PORT_STRUCTURE_DEFINITION
+
+//==============================================================================
 // Include files
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include "Switch_Link_Node.h"
+
+#if !defined (SHARE_DIO_PORT_STRUCTURE_DEFINITION)
+	#include "Switch_Link_Node.h"
+#else
+	#include "../MCTBox_API_DIO.h"
+	#include "../MCTBox_DIO/DIO_Link_Node.h"
+#endif
 
 //==============================================================================
 // Constants
@@ -23,42 +33,45 @@
 // Types
 
 //==============================================================================
-// Static global variables
-static PSwitchLNode_t pSwitchNode = NULL;
+// Global variables
+#if !defined (SHARE_DIO_PORT_STRUCTURE_DEFINITION)
+	static PSwitchLNode_t pSwitchNode = NULL;
+#else
+	extern PDioPortNode_t pDioPortNode;
+#endif
 
 //==============================================================================
 // Static functions
 
 //==============================================================================
-// Global variables
-
-//==============================================================================
 // Functions implementation :
-void AssignSwitch(PSwitchLNode_t	lpSwitchNode,
-				  UCHAR				sSwitchTokenName[], 
-				  UCHAR 			sSwitchDescription[],
-				  UCHAR 			sSwitchConnector[],
-				  UCHAR 			sSwitchDeviceName[],
-				  BYTE				byteSwitchBoardID,
-				  UINT				uiSwitchChannelNr)
-{			
-	// Assign the "Switch Token Name"
-	lpSwitchNode->sSwitchTokenName = (UCHAR *)malloc(sizeof(UCHAR) * (strlen(sSwitchTokenName)+1));
-	sprintf(lpSwitchNode->sSwitchTokenName, "%s", sSwitchTokenName);
-	// Assign the "Switch Description"
-	lpSwitchNode->sSwitchDescription = (UCHAR *)malloc(sizeof(UCHAR) * (strlen(sSwitchDescription)+1));
-	sprintf(lpSwitchNode->sSwitchDescription, "%s", sSwitchDescription);
-	// Assign the "Switch Connector"
-	lpSwitchNode->sSwitchConnector = (UCHAR *)malloc(sizeof(UCHAR) * (strlen(sSwitchConnector)+1));
-	sprintf(lpSwitchNode->sSwitchConnector, "%s", sSwitchConnector);
-	// Assign the "Switch Device Name"
-	lpSwitchNode->sSwitchDeviceName = (UCHAR *)malloc(sizeof(UCHAR) * (strlen(sSwitchDeviceName)+1));
-	sprintf(lpSwitchNode->sSwitchDeviceName, "%s", sSwitchDeviceName);
-	// Assign the "Switch Board ID"
-	lpSwitchNode->tSwitchPhyAddress.ucSwitchBoardID = byteSwitchBoardID;
-	// Assign the "Switch Channel Number"
-	lpSwitchNode->tSwitchPhyAddress.uiSwitchChannelNr = uiSwitchChannelNr;
-}
+#if !defined (SHARE_DIO_PORT_STRUCTURE_DEFINITION)
+	void AssignSwitch(PSwitchLNode_t	lpSwitchNode,
+					  UCHAR				sSwitchTokenName[], 
+					  UCHAR 			sSwitchDescription[],
+					  UCHAR 			sSwitchConnector[],
+					  UCHAR 			sSwitchDeviceName[],
+					  BYTE				byteSwitchBoardID,
+					  UINT				uiSwitchChannelNr)
+	{			
+		// Assign the "Switch Token Name"
+		lpSwitchNode->sSwitchTokenName = (UCHAR *)malloc(sizeof(UCHAR) * (strlen(sSwitchTokenName)+1));
+		sprintf(lpSwitchNode->sSwitchTokenName, "%s", sSwitchTokenName);
+		// Assign the "Switch Description"
+		lpSwitchNode->sSwitchDescription = (UCHAR *)malloc(sizeof(UCHAR) * (strlen(sSwitchDescription)+1));
+		sprintf(lpSwitchNode->sSwitchDescription, "%s", sSwitchDescription);
+		// Assign the "Switch Connector"
+		lpSwitchNode->sSwitchConnector = (UCHAR *)malloc(sizeof(UCHAR) * (strlen(sSwitchConnector)+1));
+		sprintf(lpSwitchNode->sSwitchConnector, "%s", sSwitchConnector);
+		// Assign the "Switch Device Name"
+		lpSwitchNode->sSwitchDeviceName = (UCHAR *)malloc(sizeof(UCHAR) * (strlen(sSwitchDeviceName)+1));
+		sprintf(lpSwitchNode->sSwitchDeviceName, "%s", sSwitchDeviceName);
+		// Assign the "Switch Board ID"
+		lpSwitchNode->tSwitchPhyAddress.ucSwitchBoardID = byteSwitchBoardID;
+		// Assign the "Switch Channel Number"
+		lpSwitchNode->tSwitchPhyAddress.uiSwitchChannelNr = uiSwitchChannelNr;
+	}
+#endif
 
 /******************************************************************************/
 int API MCTBoxAPI_SwitchModule_SwitchListAddEntry(UCHAR sSwitchTokenName[], 
@@ -68,177 +81,277 @@ int API MCTBoxAPI_SwitchModule_SwitchListAddEntry(UCHAR sSwitchTokenName[],
 												  BYTE	byteSwitchBoardID,
 												  UINT	uiSwitchChannelNr)
 {
-	PSwitchLNode_t pCurrentNode, pTemp = pSwitchNode, pNewNode = NULL;
+	int iResult = 0;
+	#if !defined (SHARE_DIO_PORT_STRUCTURE_DEFINITION)
+		PSwitchLNode_t pCurrentNode, pTemp = pSwitchNode, pNewNode = NULL;
 	
-	if (pSwitchNode == NULL)
-	{
-		pSwitchNode = (PSwitchLNode_t)malloc(sizeof(SwitchLNode_t));	// Create the head node
-		if (pSwitchNode)
+		if (pSwitchNode == NULL)
 		{
-			// Assign the Switch :
-			AssignSwitch(pSwitchNode,
-						 sSwitchTokenName,
-						 sSwitchDescription,
-						 sSwitchConnector,
-						 sSwitchDeviceName,
-						 byteSwitchBoardID,
-						 uiSwitchChannelNr);
-			// Assign the next node to NULL.
-			pSwitchNode->next = NULL;
+			pSwitchNode = (PSwitchLNode_t)malloc(sizeof(SwitchLNode_t));	// Create the head node
+			if (pSwitchNode)
+			{
+				// Assign the Switch :
+				AssignSwitch(pSwitchNode,
+							 sSwitchTokenName,
+							 sSwitchDescription,
+							 sSwitchConnector,
+							 sSwitchDeviceName,
+							 byteSwitchBoardID,
+							 uiSwitchChannelNr);
+				// Assign the next node to NULL.
+				pSwitchNode->next = NULL;
 			
-			return 0;	// OK
+				return 0;	// OK
+			}
+			else
+			{
+				return -2;	// Memory overflow
+			}
+		}
+	
+		/* 
+		 * Append a new tail node
+		 */
+		do
+		{
+			pCurrentNode = pTemp;	// Stage current node
+			pTemp = pTemp->next;
+		}
+		while (pTemp != NULL);	// Move to the tail node
+	
+		pNewNode = (PSwitchLNode_t)malloc(sizeof(SwitchLNode_t));	// Create the new node
+		if (pNewNode)
+		{
+			AssignSwitch(pNewNode, 
+						 sSwitchTokenName, 
+						 sSwitchDescription, 
+						 sSwitchConnector, 
+						 sSwitchDeviceName, 
+						 byteSwitchBoardID, 
+						 uiSwitchChannelNr);
+			pNewNode->next = NULL;
 		}
 		else
 		{
-			return -2;	// Memory overflow
+			return -2;
 		}
-	}
-	
-	/* 
-	 * Append a new tail node
-	 */
-	do
-	{
-		pCurrentNode = pTemp;	// Stage current node
-		pTemp = pTemp->next;
-	}
-	while (pTemp != NULL);	// Move to the tail node
-	
-	pNewNode = (PSwitchLNode_t)malloc(sizeof(SwitchLNode_t));	// Create the new node
-	if (pNewNode)
-	{
-		AssignSwitch(pNewNode, 
-					 sSwitchTokenName, 
-					 sSwitchDescription, 
-					 sSwitchConnector, 
-					 sSwitchDeviceName, 
-					 byteSwitchBoardID, 
-					 uiSwitchChannelNr);
-		pNewNode->next = NULL;
-	}
-	else
-	{
-		return -2;
-	}
-	pCurrentNode->next = pNewNode;	// Insert as the tail node.
-	return 0;
+		pCurrentNode->next = pNewNode;	// Insert as the tail node.
+	#else					
+		iResult = MCTBoxAPI_DIOModule_DoutPortListAddEntry(sSwitchTokenName,
+														   sSwitchDescription,
+														   sSwitchConnector,
+														   sSwitchDeviceName,
+														   byteSwitchBoardID,
+														   uiSwitchChannelNr);
+	#endif
+	return iResult;
 }
 
 int API MCTBoxAPI_SwitchModule_SwitchDeleteList(void)
 {
-	PSwitchLNode_t pTemp = pSwitchNode;
+	int iResult = 0;
+	#if !defined (SHARE_DIO_PORT_STRUCTURE_DEFINITION)
+		PSwitchLNode_t pTemp = pSwitchNode;
 	
-	while (NULL != pTemp)
-	{					
-		pTemp = pTemp->next;	// Stage the new node
-		free(pSwitchNode);		// Destroy current node
-		pSwitchNode = pTemp;	// Move to next node
-	}
-	if (NULL != pSwitchNode)
-		free(pSwitchNode);			// Destroy the tail node
-	
-	return 0;
+		while (NULL != pTemp)
+		{					
+			pTemp = pTemp->next;	// Stage the new node
+			free(pSwitchNode);		// Destroy current node
+			pSwitchNode = pTemp;	// Move to next node
+		}
+		if (NULL != pSwitchNode)
+			free(pSwitchNode);			// Destroy the tail node
+	#else
+		iResult = MCTBoxAPI_DIOModule_DioPortDeleteList();
+	#endif
+	return iResult;
 }
 
 /******************************************************************************/
 int API MCTBoxAPI_SwitchModule_SetSwitch(UCHAR sSwitchTokenName[], char *sErrorMesg)
 {
-	PSwitchLNode_t pTemp = pSwitchNode;
-	int iHitTargetFlag = 0, iError = 0;
-	char sError[256] = {0};
+	int iResult = 0;
+	#if !defined (SHARE_DIO_PORT_STRUCTURE_DEFINITION)
+		PSwitchLNode_t pTemp = pSwitchNode;
+		int iHitTargetFlag = 0, iError = 0;
+		char sError[256] = {0};
 	
-	while(pTemp != NULL)
-	{
-		if (!strcmp(pTemp->sSwitchTokenName, sSwitchTokenName))
+		while(pTemp != NULL)
 		{
-			iHitTargetFlag = 1;
-			iError = MCTBoxAPI_SwitchModule_OpenCloseSwitch(pTemp->tSwitchPhyAddress.ucSwitchBoardID,
-															pTemp->tSwitchPhyAddress.uiSwitchChannelNr,
-															ON,
-															sError);
-			if (iError)
+			if (!strcmp(pTemp->sSwitchTokenName, sSwitchTokenName))
 			{
-				sprintf(sErrorMesg, "%s", sError);
-				return iError;
+				iHitTargetFlag = 1;
+				iError = MCTBoxAPI_SwitchModule_OpenCloseSwitch(pTemp->tSwitchPhyAddress.ucSwitchBoardID,
+																pTemp->tSwitchPhyAddress.uiSwitchChannelNr,
+																ON,
+																sError);
+				if (iError)
+				{
+					sprintf(sErrorMesg, "%s", sError);
+					return iError;
+				}
 			}
-		}
 		
-		pTemp = pTemp->next;	// next node
-	}
-	if (iHitTargetFlag != 1)	// Check the token existed.
-	{
-		sprintf(sErrorMesg, "No '%s' switch token existed", sSwitchTokenName);
-		return -10;		// Token NOT found.
-	}
+			pTemp = pTemp->next;	// next node
+		}
+		if (iHitTargetFlag != 1)	// Check the token existed.
+		{
+			sprintf(sErrorMesg, "No '%s' switch token existed", sSwitchTokenName);
+			return -10;		// Token NOT found.
+		}
+	#else
+		PDioPortNode_t pTemp = pDioPortNode;
+		int iHitTargetFlag = 0;
+		char sError[256] = {0};
 	
-	return 0;
+		while (pTemp != NULL)
+		{
+			if (!strcmp(pTemp->stDioPortToken.sDioPortTokenName, sSwitchTokenName))
+			{
+				iHitTargetFlag = 1;
+				iResult = MCTBoxAPI_SwitchModule_OpenCloseSwitch(pTemp->stDioPortPhyAddr.ucDioBoardID,
+																 pTemp->stDioPortPhyAddr.uiDioPortNr,
+																 ON,
+																 sError);
+				if (iResult)
+				{
+					sprintf(sErrorMesg, "%s", sError);
+					return iResult;
+				}
+			}
+			pTemp = pTemp->next;
+		}
+		if (iHitTargetFlag != 1)
+		{
+			sprintf(sErrorMesg, "No '%s' switch token existed", sSwitchTokenName);
+			return -10;
+		}
+	#endif
+	
+	return iResult;
 }
 
 /******************************************************************************/
 int API MCTBoxAPI_SwitchModule_ResetSwitch(UCHAR sSwitchTokenName[], char *sErrorMesg)
 {
-	PSwitchLNode_t pTemp = pSwitchNode;
-	int iHitTargetFlag = 0, iError = 0;
-	char sError[256] = {0};
+	int iResult = 0;
+	#if !defined (SHARE_DIO_PORT_STRUCTURE_DEFINITION)
+		PSwitchLNode_t pTemp = pSwitchNode;
+		int iHitTargetFlag = 0, iError = 0;
+		char sError[256] = {0};
 	
-	while(pTemp != NULL)
-	{
-		if (!strcmp(pTemp->sSwitchTokenName, sSwitchTokenName))
+		while(pTemp != NULL)
 		{
-			iHitTargetFlag = 1;
-			iError = MCTBoxAPI_SwitchModule_OpenCloseSwitch(pTemp->tSwitchPhyAddress.ucSwitchBoardID,
-															pTemp->tSwitchPhyAddress.uiSwitchChannelNr,
-															OFF,
-															sError);
-			if (iError)
+			if (!strcmp(pTemp->sSwitchTokenName, sSwitchTokenName))
 			{
-				sprintf(sErrorMesg, "%s", sError);
-				return iError;
+				iHitTargetFlag = 1;
+				iError = MCTBoxAPI_SwitchModule_OpenCloseSwitch(pTemp->tSwitchPhyAddress.ucSwitchBoardID,
+																pTemp->tSwitchPhyAddress.uiSwitchChannelNr,
+																OFF,
+																sError);
+				if (iError)
+				{
+					sprintf(sErrorMesg, "%s", sError);
+					return iError;
+				}
 			}
-		}
 		
-		pTemp = pTemp->next;	// next node
-	}
-	if (iHitTargetFlag != 1)	// Check the token existed.
-	{
-		sprintf(sErrorMesg, "Unknown '%s' switch token.", sSwitchTokenName);
-		return -10;		// Token NOT found.
-	}
+			pTemp = pTemp->next;	// next node
+		}
+		if (iHitTargetFlag != 1)	// Check the token existed.
+		{
+			sprintf(sErrorMesg, "Unknown '%s' switch token.", sSwitchTokenName);
+			return -10;		// Token NOT found.
+		}
+	#else
+		PDioPortNode_t pTemp = pDioPortNode;
+		int iHitTargetFlag = 0;
+		char sError[256] = {0};
 	
+		while (pTemp != NULL)
+		{
+			if (!strcmp(pTemp->stDioPortToken.sDioPortTokenName, sSwitchTokenName))
+			{
+				iHitTargetFlag = 1;
+				iResult = MCTBoxAPI_SwitchModule_OpenCloseSwitch(pTemp->stDioPortPhyAddr.ucDioBoardID,
+																 pTemp->stDioPortPhyAddr.uiDioPortNr,
+																 OFF,
+																 sError);
+				if (iResult)
+				{
+					sprintf(sErrorMesg, "%s", sError);
+					return iResult;
+				}
+			}
+			pTemp = pTemp->next;
+		}
+		if (iHitTargetFlag != 1)
+		{
+			sprintf(sErrorMesg, "Unknown '%s' switch token.", sSwitchTokenName);
+			return -10;
+		}
+	#endif
 	return 0;
 }
 
 /******************************************************************************/
 int API MCTBoxAPI_SwitchModule_OutValve(char sSwitchTokenName[], EState eValveState, char *sErrorMesg)
 {
-	int iResult = 0;
+	int iResult = 0;   
+	#if !defined (SHARE_DIO_PORT_STRUCTURE_DEFINITION)
+		PSwitchLNode_t pTemp = pSwitchNode;
+		int iHitTargetFlag = 0;
+		char sError[256] = {0};
 	
-	PSwitchLNode_t pTemp = pSwitchNode;
-	int iHitTargetFlag = 0;
-	char sError[256] = {0};
-	
-	while (pTemp != NULL)
-	{
-		if (!strcmp(pTemp->sSwitchTokenName, sSwitchTokenName))
+		while (pTemp != NULL)
 		{
-			iHitTargetFlag = 1;
-			iResult = MCTBoxAPI_SwitchModule_OpenCloseSwitch(pTemp->tSwitchPhyAddress.ucSwitchBoardID,
-															 pTemp->tSwitchPhyAddress.uiSwitchChannelNr,
-															 eValveState,
-															 sError);
+			if (!strcmp(pTemp->sSwitchTokenName, sSwitchTokenName))
+			{
+				iHitTargetFlag = 1;
+				iResult = MCTBoxAPI_SwitchModule_OpenCloseSwitch(pTemp->tSwitchPhyAddress.ucSwitchBoardID,
+																 pTemp->tSwitchPhyAddress.uiSwitchChannelNr,
+																 eValveState,
+																 sError);
+			}
+			if (iResult)
+			{
+				sprintf(sErrorMesg, "%s", sError);
+				return iResult;
+			}
+			pTemp = pTemp->next;	// next node
 		}
-		if (iResult)
+		if (iHitTargetFlag != 1)
 		{
-			sprintf(sErrorMesg, "%s", sError);
-			return iResult;
-		}
-		pTemp = pTemp->next;	// next node
-	}
-	if (iHitTargetFlag != 1)
-	{
-		sprintf(sErrorMesg, "Unknown '%s' switch token.", sSwitchTokenName);
-		return -10;		// Token NOT found.
-	}
+			sprintf(sErrorMesg, "Unknown '%s' switch token.", sSwitchTokenName);
+			return -10;		// Token NOT found.
+		}											
+	#else
+		PDioPortNode_t pTemp = pDioPortNode;
+		int iHitTargetFlag = 0;
+		char sError[256] = {0};
 	
+		while (pTemp != NULL)
+		{
+			if (!strcmp(pTemp->stDioPortToken.sDioPortTokenName, sSwitchTokenName))
+			{
+				iHitTargetFlag = 1;
+				iResult = MCTBoxAPI_SwitchModule_OpenCloseSwitch(pTemp->stDioPortPhyAddr.ucDioBoardID,
+																 pTemp->stDioPortPhyAddr.uiDioPortNr,
+																 eValveState,
+																 sError);
+				if (iResult)
+				{
+					sprintf(sErrorMesg, "%s", sError);
+					return iResult;
+				}
+			}
+			pTemp = pTemp->next;
+		}
+		if (iHitTargetFlag != 1)
+		{
+			sprintf(sErrorMesg, "Unknown '%s' switch token.", sSwitchTokenName);
+			return -10;
+		}
+	#endif
 	return iResult;
 }
